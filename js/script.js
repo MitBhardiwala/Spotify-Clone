050d36f060429ec4fe6b48f2f6b05fe2141cadde
MitBhardiwala <112770042+MitBhardiwala@users.noreply.github.com>
2024-05-20 18:39:03 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ let prevVol;
 
 async function getSongs(folder) {
     currFolder = folder;
-    let a = await fetch(`http://127.0.0.1:5500/${folder}/`)
+    let a = await fetch(`/${folder}/`)
     let response = await a.text();
     // console.log(response);
 
